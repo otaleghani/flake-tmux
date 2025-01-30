aeffd35930ec27454579fd4ffcd2561929857c9c
Oliviero Taleghani <o.taleghani@talesign.com>
2025-01-30 11:00:09 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -3,25 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      tmuxConfig = ''
+        set -g status-bg colour235
+        set -g status-fg colour136
+      '';
+
+      tmuxWithConfig = pkgs.tmux.overrideAttrs (old: {
+        postInstall = ''
+          mkdir -p $out/etc/tmux
+          echo "${tmuxConfig}" > $out/etc/tmux/tmux.conf
+        '';
+      });
     in
     {
-      # devShells.${system}.default =
-      default =
-        pkgs.mkShell {
-          buildInputs = [
-            pkgs.tmux
-          ];
 
-          shellHook = ''
-            echo "Hello from tmux derivation"
-          '';
-        };
+      # default =
+      #   pkgs.mkShell {
+      #     buildInputs = [
+      #       pkgs.tmux
+      #     ];
+
+      #     shellHook = ''
+      #       echo "Hello from tmux derivation"
+      #     '';
+      #   };
+
+      packages.${system} = {
+        tmuxWithConfig = tmuxWithConfig;
+        default = tmuxWithConfig;
+        # pkgs.tmux;
+      };
     };
 }
