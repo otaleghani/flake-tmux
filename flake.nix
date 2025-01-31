3ead7dc0e858f586d7755cf06a0be62c6ba0479f
Oliviero Taleghani <o.taleghani@talesign.com>
2025-01-31 16:23:45 +0000

    backup

--- flake.nix
+++ flake.nix
@@ -15,21 +15,18 @@
       set -g status-fg colour136
     '';
 
-    tmuxWithConfig = pkgs.tmux.overrideAttrs (old: {
-      postInstall = ''
-        mkdir -p $out/etc/tmux
-        echo "${tmuxConfig}" > $out/etc/tmux/tmux.conf
-      '';
-    });
+    # tmuxWithConfig = pkgs.tmux.overrideAttrs (old: {
+    #   postInstall = ''
+    #     mkdir -p $out/etc/tmux
+    #     echo "${tmuxConfig}" > $out/etc/tmux/tmux.conf
+    #   '';
+    # });
   in
   {
-    # packages.${system}.default = pkgs.mkShell {
-    #   packages = [ pkgs.tmux ];
-    # };
-
     devShells.${system}.default = pkgs.mkShell {
       packages = [ pkgs.tmux ];
       shellHook = ''
+        echo "${tmuxConfig}" > ./tmux.conf
         echo "helo"
       '';
     };
